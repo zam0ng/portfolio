a3de371143395eb2fbc839555a40f2d0e3852575
zamong <sinze27@gmail.com>
2025-02-05 18:52:46 +0900

    edit

--- src/contents/introduce.jsx
+++ src/contents/introduce.jsx
@@ -16,7 +16,7 @@ const Introduce = () => {
             <div className='-ml-52 text-3xl font-bold'>About me</div>
                 <div className="text-left text-[16px] md:text-[24px] ml-80 font-ridi">
                     <ul className="px-[32px] leading-8 list-disc list-outside text-[12px] md:text-[16px]">
-                        <li>안녕하세요 ! 신입 블록체인 개발자 이재영입니다.</li>
+                        <li>안녕하세요 ! 개발자 이재영입니다.</li>
                         <li>긍정적인 성격으로 배움에 두려움이 없으며, 
                              유연한 사고로 받아들이는 것에 거부감이 없습니다. </li>
                         <li>같은 실수를 반복하지 않기 위해 새롭게 알게된 정보를 꾸준히 블로그와 노트에 기록하고 있습니다.</li>
